fafe2603d46226059b07ed1a2b5f8aee8f32f4c0
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-13 08:10:10 +0300

    added bootstrap to installed apps

--- awwardsprj/settings.py
+++ awwardsprj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apawwards',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
