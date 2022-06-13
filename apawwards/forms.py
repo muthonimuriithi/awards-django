6d8521748b99471b13577b983d9a2b04daf0dbf2
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-13 23:10:37 +0300

    created view for form html

---
+++ apawwards/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Profile,Projects,Rates,Comments
+
+
+class ProjectUploadForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        fields = ('title','image_landing','description', 'link')
+        
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo','bio','website') 
+        
+class VotesForm(forms.ModelForm):
+    class Meta:
+        model = Rates
+        fields = ('design','usability','content')
+        
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('comments',)
