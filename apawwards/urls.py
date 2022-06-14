f4bba79138563799702fc7c8e78ad11ef3b06141
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-15 01:35:37 +0300

    fx: adjusted the projects path

--- apawwards/urls.py
+++ apawwards/urls.py
@@ -8,8 +8,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('projects/(\d+)',views.projects,name='projects'),
-    path('profile/(?P<username>\w+)', views.profile, name='profile'),
+    path('projects/<int:pk>/',views.projects,name='projects'),
+    path('profile<username>/', views.profile, name='profile'),
     path('uploads/',views.post_site,name='post_site'),
     path('api/profiles/', views.ProfileList.as_view(),name='profile_list'),
     path('api/projects/', views.ProjectsList.as_view(),name='projects_list'),
