5d6f9e77897d5e82172755d1389842c10bb4498b
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-15 16:23:26 +0300

    linked my app to the project urls

--- awwardsprj/urls.py
+++ awwardsprj/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apawwards.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(), {"next_page": '/'}), 
+    path('api-token-auth/', obtain_auth_token)
 ]
