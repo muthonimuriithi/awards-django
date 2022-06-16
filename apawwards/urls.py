287c4ec0aeb7fb30173918a63081db1597c6631e
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-16 17:47:37 +0300

    settings adjustments

--- apawwards/urls.py
+++ apawwards/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from . import settings
+from awwardsprj import settings
 from django.conf.urls.static import  static
 
 
